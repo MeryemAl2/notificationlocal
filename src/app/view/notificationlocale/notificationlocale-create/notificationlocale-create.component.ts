ab467f7ed6c628d565d4792dab128d8d4c4f9323
MeryemAl2 <125358759+MeryemAl2@users.noreply.github.com>
2023-04-01 18:25:32 +0000

    notif

--- src/app/view/notificationlocale/notificationlocale-create/notificationlocale-create.component.ts
+++ src/app/view/notificationlocale/notificationlocale-create/notificationlocale-create.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {NotificationLocale} from "../../../controler/model/notificationLocale";
 import {NotificationLocaleService} from "src/app/controler/service/notification-locale.service";
 import {Notif} from "../../../controler/model/notif";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-notificationlocale-create',
@@ -10,29 +11,39 @@ import {Notif} from "../../../controler/model/notif";
 })
 export class NotificationlocaleCreateComponent {
 
-notif = new Notif();
-result: any;
+  notif = new Notif();
+  result: any;
 
   constructor(private notificationlocaleservice: NotificationLocaleService) {
   }
 
 
-  public creerNotif(): void{
-   this.notificationlocaleservice.creernotifs(this.notif).subscribe((resp:number)=>{
-     if(resp==-1){
-       this.result="locals n'existe pas";
+  public creerNotif(): void {
+    this.notificationlocaleservice.creernotifs(this.notif).subscribe((resp: number) => {
+        if (resp == -1) {
+          this.result = "locals n'existe pas";
 
-     }if(resp==-2){
-       this.result="redevable n'existe pas";
-     }
-   })
+        }if(resp==1){
+          alert("saved")
+        }
+        if (resp == -2) {
+          this.result = "redevable n'existe pas";
+        }else{
+          alert("babla");
+        }
+
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      })
   }
+
   get notificationlocale(): NotificationLocale {
     return this.notificationlocaleservice.notificationlocale;
   }
 
   set notificationlocale(value: NotificationLocale) {
-    this.notificationlocaleservice.notificationlocale  = value;
+    this.notificationlocaleservice.notificationlocale = value;
   }
 
 
