91ea05ef2bec60f5a8f82849a1fd385dda0672c1
MeryemAl2 <125358759+MeryemAl2@users.noreply.github.com>
2023-04-05 17:21:00 +0000

    notifi

--- src/app/controler/service/notification-locale.service.ts
+++ src/app/controler/service/notification-locale.service.ts
@@ -9,7 +9,9 @@ import {Notif} from "../model/notif";
 })
 export class NotificationLocaleService {
   private _notificationlocale= new NotificationLocale() ;
+  private _notificationlocales = new Array<NotificationLocale>;
   private _url = 'http://localhost:8036/api/v1/notificationlocale/';
+  private _url2 = 'http://localhost:8036/api/v1/notification/';
 
   constructor(private http: HttpClient) {}
   get notificationlocale(): NotificationLocale {
@@ -18,17 +20,27 @@ export class NotificationLocaleService {
     }
     return this._notificationlocale;
   }
-
   set notificationlocale(value: NotificationLocale) {
     this._notificationlocale = value;
   }
+  get notificationlocales(): NotificationLocale[] {
+    return this._notificationlocales;
+  }
 
+  set notificationlocales(value: NotificationLocale[]) {
+    this._notificationlocales = value;
+  }
+  saveNotif(notif: Notif): Observable<number> {
+    return this.http.post<number>(this._url2,notif);
+  }
   public creernotifs(notif: Notif): Observable<number>{
     return this.http.post<number>(this._url, notif);
   }
 
 
-
+  public findAll(): Observable<Array<NotificationLocale>> {
+     return this.http.get<Array<NotificationLocale>>(this._url);
+  }
 
 
 }
--- src/app/view/notificationlocale/notificationlocale-create/notificationlocale-create.component.ts
+++ src/app/view/notificationlocale/notificationlocale-create/notificationlocale-create.component.ts
@@ -10,30 +10,53 @@ import {Notif} from "../../../controler/model/notif";
 })
 export class NotificationlocaleCreateComponent {
 
-notif = new Notif();
-result: any;
+  notif = new Notif();
+  result: any;
 
   constructor(private notificationlocaleservice: NotificationLocaleService) {
   }
 
 
-  public creerNotif(): void{
-   this.notificationlocaleservice.creernotifs(this.notif).subscribe((resp:number)=>{
-     if(resp==-1){
-       this.result="locals n'existe pas";
+  public saveNotif(): void {
+    this.notificationlocaleservice.saveNotif(this.notif).subscribe((resp: number) => {
+      if (resp == 1) {
+        this.notificationlocaleservice.creernotifs(this.notif).subscribe((resp: number) => {
+          if (resp != 1) {
+            this.result = "notification dupliquee";
+          }
+        })
+        alert("saved");
+      }
+      if (resp == -3) {
+        alert("locals n'existe pas");
+      }
+      if (resp == -2) {
+        alert("notification est deja envoyee");
+      }
+      if (resp == -1) {
+        alert("notification == 0") ;
+      }
 
-     }if(resp==-2){
-       this.result="redevable n'existe pas";
-     }
-   })
+      if (resp == -4) {
+        alert("redevable n'existe pas");
+      }
+      this.notificationlocales.push({...this.notificationlocale});
+    })
   }
+
   get notificationlocale(): NotificationLocale {
     return this.notificationlocaleservice.notificationlocale;
   }
 
   set notificationlocale(value: NotificationLocale) {
-    this.notificationlocaleservice.notificationlocale  = value;
+    this.notificationlocaleservice.notificationlocale = value;
+  }
+  get notificationlocales(): NotificationLocale[] {
+    return this.notificationlocaleservice.notificationlocales;
   }
 
+  set notificationlocales(value: NotificationLocale[]) {
+    this.notificationlocaleservice.notificationlocales = value;
+  }
 
 }
